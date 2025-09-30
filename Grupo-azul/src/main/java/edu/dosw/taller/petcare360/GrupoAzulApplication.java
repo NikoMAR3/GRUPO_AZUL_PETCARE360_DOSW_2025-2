87a7a176cbb208bc039c6fba1cac0082836b2f22
Nikolas <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-09-29 22:52:52 -0500

    Initial commit con estrucutra de scaffolding

---
+++ Grupo-azul/src/main/java/edu/dosw/taller/petcare360/GrupoAzulApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller.petcare360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoAzulApplication.class, args);
+	}
+
+}
---
+++ Grupo-azul/src/test/java/edu/dosw/taller/petcare360/GrupoAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller.petcare360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
